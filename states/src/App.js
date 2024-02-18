c27ccece732e6b196582f02b99d50afc2becfe4e
nurselcansuozkan <nurselcansuozkan@gmail.com>
2024-02-18 18:05:45 +0300

    Array states kullanımı

--- states/src/App.js
+++ states/src/App.js
@@ -1,14 +1,24 @@
 import { useState } from "react";
 function App() {
- //state:değerinin değişme potansiyeli olan bütün verileri tutan yap
-  const [name, setName] = useState('Nursel');
-  const [age,setAge]= useState('32');
+  //state:değerinin değişme potansiyeli olan bütün verileri tutan yap
+  const [name, setName] = useState("Nursel");
+  const [age, setAge] = useState("32");
+  const[friends, setFriends] = useState(["Nursel", "Osman"]);
   return (
     <div className="App">
       <h1>Merhaba {name}!</h1>
       <h2>{age}</h2>
-      <button onClick={()=>setName('Cansu')}>Change Name</button>
-      <button onClick={()=>setAge(27)}>Change Age</button>
+      <button onClick={() => setName("Cansu")}>Change Name</button>
+      <button onClick={() => setAge(27)}>Change Age</button>
+
+      <hr />
+      <h2>Friends</h2>
+      {friends.map((friend, index) => (
+        <div key={index}>{friend}</div>
+      ))}
+      <br/>
+      //array içerisinde önceki verileri korumak istiyorsak ... (spread operatörü) kullanıyoruz
+      <button onClick={()=>setFriends(["Cansu",...friends])}>Add new friend</button>
     </div>
   );
 }
